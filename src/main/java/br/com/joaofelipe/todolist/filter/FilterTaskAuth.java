f089d2a938571cd520f2afcf22cd1f4c16828765
João Felipe Manferrari <jmanferrari@gmail.com>
2026-02-05 15:41:30 -0300

    feat : validação da senha e do usuario em FilterTaskAuth.java

--- src/main/java/br/com/joaofelipe/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/joaofelipe/todolist/filter/FilterTaskAuth.java
@@ -3,10 +3,12 @@ package br.com.joaofelipe.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.joaofelipe.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,25 +17,35 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter{
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException { 
+
+                //pega o caminho da requisição somente para /tasks/
+                var servletPath = request.getServletPath();
+
+                //verifica se o caminho é /tasks/, caso seja aplica a autenticação
+                if(servletPath.equals("/tasks/")){
+
                 
-                //pegar a autenticação do usuário
+                //pega a autenticação do usuário
+
                var Authorization = request.getHeader("Authorization");
                
                 // separa a palavra Basic do restante da string
 
                var authEncoded = Authorization.substring("Basic".length()).trim();
 
-            
                 //Decodifica a string que está em Base64 para um array de bytes
 
                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
 
                //converte o array de bytes para string
 
-               var authString =new String(authDecode);
+               var authString = new String(authDecode);
             
 
                //cria um array para serparar o username e a senha, com o usuario estando
@@ -43,17 +55,27 @@ public class FilterTaskAuth extends OncePerRequestFilter{
                String username = credentials[0];
                 String password = credentials[1];
 
-                //Exibe no console o usuário e a senha após a mensagem de autorizado
+                //valida se o usuário existe no banco de dados, caso não exista retorna um erro 401, caso exista continua a requisição normalmente
 
-                System.out.println("Autorizado pelo filtro");
-               System.out.println("Usuário: " + username);
-               System.err.println("Senha: " + password);
+               var user = this.userRepository.findByUsername(username);
+               if(user == null){
+                response.sendError(401);
+
+               } else {
+                //valida a senha do usuario
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
                 
+                if(passwordVerify.verified){
+
+                filterChain.doFilter(request, response);   
+
+               } else{
+                response.sendError(401);
+               }}
+
+            }else{
                 filterChain.doFilter(request, response);
-        
-    }
+            }
 
-    
-
-    
+            }
 }
